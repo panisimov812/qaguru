877cde6469e73c120755ec31e0d52f9843f9d517
panisimov812 <panisimov@gid.ru>
2022-11-03 22:14:53 +0300

    add new line about flow

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("Hello GIT2");
-
+        System.out.println("flow branch");
     }
 }
